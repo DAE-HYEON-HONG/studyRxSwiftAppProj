3e440579a80d6d04e8b8200eb239b532122ab143
hong4383@naver.com <hong4383@naver.com>
2022-05-22 01:58:11 +0200

    [feat] made UIColor HexColor code option and loading screen(making)

---
+++ MVVMmySelf/view/LoadingVC.swift
@@ -0,0 +1,27 @@
+//
+//  LoadingVC.swift
+//  MVVMmySelf
+//
+//  Created by HongDaehyeon on 2022/05/20.
+// MakeLoading Screen with Animation
+// Use LoadingVCModell in viewModel Group Folder
+
+import UIKit
+
+class LoadingVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBOutlet weak var mainLogo: UIView!{
+        didSet{
+            self.mainLogo.layer.cornerRadius = 8
+            self.mainLogo.clipsToBounds = true
+            self.mainLogo.backgroundColor = 
+        }
+    }
+    
+
+}
+
---
+++ MVVMmySelf/view/viewModel/LoadingVCModel.swift
@@ -0,0 +1,14 @@
+//
+//  LoadingVCModel.swift
+//  MVVMmySelf
+//
+//  Created by HongDaehyeon on 2022/05/22.
+//
+
+import Foundation
+import RxCocoa
+import RxSwift
+
+struct LoadingVCModel {
+    
+}
